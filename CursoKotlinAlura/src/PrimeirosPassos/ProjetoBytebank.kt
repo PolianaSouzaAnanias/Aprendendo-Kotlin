f22291fd3ae18cb08838c87d8f772e59a7017323
PolianaSouzaAnanias <poliana.souzaa@hotmail.com>
2021-10-21 21:33:54 -0300

    If, else e while (Condições e Laços)

--- CursoKotlinAlura/src/PrimeirosPassos/ProjetoBytebank.kt
+++ CursoKotlinAlura/src/PrimeirosPassos/ProjetoBytebank.kt
@@ -8,6 +8,32 @@ class ProjetoBytebank {
         var titular = ""
         var numeroConta = 0
         var saldoConta = 0.0
-    }
+	}
+		
+		fun testesLacos(){
+    var i = 0
+    while(i < 5){
+        val titularConta: String = "Poliana $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
+
+        println("Olá, $titularConta")
+        println("Número da sua conta $numeroConta")
+        println("Seu saldo é $saldo")
+        println()
+        i++
+
+        testarCondicoes(saldo)
+		
+		if (saldo > 0.0){
+         println("Saldo positivo")
+     } else if (saldo == 0.0){
+         println("Saldo neltro")
+     } else{
+         println("Saldo negativo")
+     }
+	}
+		}
+	
 	}
 }
