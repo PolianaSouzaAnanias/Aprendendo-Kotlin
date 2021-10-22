dece123286a9a4a12a9439f81aa541544e94fffc
PolianaSouzaAnanias <poliana.souzaa@hotmail.com>
2021-10-21 21:32:53 -0300

    if, else e while (condições e laços)

--- CursoKotlinAlura/src/PrimeirosPassos/ProjetoBytebank.kt
+++ CursoKotlinAlura/src/PrimeirosPassos/ProjetoBytebank.kt
@@ -1,4 +1,13 @@
 package PrimeirosPassos
 
 class ProjetoBytebank {
+	fun main (){
+		
+    println("Bem vindo(a) ao Bytebank!")
+    class Conta {
+        var titular = ""
+        var numeroConta = 0
+        var saldoConta = 0.0
+    }
+	}
 }
